c08a3fe801ce39ee7207658b1c1387171349e48f
Humble689 <marktravis689@gmail.com>
2026-09-20 12:28:07 +0300

    db ssl

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => \yii\db\Connection::class,
-    'dsn' => getenv('DB_DSN') ?: 'pgsql:host=localhost;port=5432;dbname=schoolpay',
+    'dsn' => getenv('DB_DSN') ?: 'pgsql:host=localhost;port=5432;dbname=schoolpay;sslmode=require',
     'username' => getenv('DB_USERNAME') ?: 'postgres',
-    'password' => getenv('DB_PASSWORD') ?: '',
+    'password' => getenv('DB_PASSWORD') ?: '',// when local, insert password here of creat an .env file for it
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
